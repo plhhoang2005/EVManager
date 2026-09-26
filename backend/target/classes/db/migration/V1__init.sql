7e39c453e6e9b099ab697088bb86faaff1295b98
Thanh Phuc <phucngo1707@gmail.com>
2026-09-26 20:49:16 +0700

    chore: remove target directory

--- backend/target/classes/db/migration/V1__init.sql
+++
@@ -1,5 +0,0 @@
--- V1__init.sql
--- Flyway migration script
--- This is a skeleton migration file. Actual tables will be created in future issues.
-
-SELECT 1;
--- backend/target/classes/db/migration/V3__update_admin_password.sql
+++
@@ -1,1 +0,0 @@
-UPDATE users SET password_hash = '$2a$10$SL9bVeTqfCi0RWrrzOIEQOzfQK1xe7RYasptYMqbeDRqfwAxtSjd6' WHERE username = 'admin';
